9fa8b9d26fe1b4ee483d7dcee716ca5565809d80
Leanpe <leanperez2112@gmail.com>
2025-05-15 21:38:22 -0300

    Base del backend

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./connection/connection");
+
+dotenv.config(); // Carga las variables del .env
+
+const app = express();
+
+// Middleware para recibir JSON
+app.use(express.json());
+
+// Conexión a MongoDB
+connectDB();
+
+// Ruta de prueba
+app.get("/", (req, res) => {
+  res.send("API funcionando correctamente 🚀");
+});
+
+// Levantar el servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+});
